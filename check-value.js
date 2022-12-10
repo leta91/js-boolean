b485ca0fb516d129f9d30bd94d00c1bd629119ee
Letizia DI MAIO <109817389+leta91@users.noreply.github.com>
2022-12-10 15:45:43 +0000

    Create check-value.js
    JS file

---
+++ check-value.js
@@ -0,0 +1,10 @@
+function targaryen(bool) {
+  if (bool === true || bool === false) {
+    return true;
+  }
+  return false;
+}
+
+console.log(targaryen(null));
+
+// the output is “false” in this case //
